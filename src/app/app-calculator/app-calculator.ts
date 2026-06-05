f050cab32cbba6f6d6bacdcf322cd4211b6f3724
oleg <oleg_09-95@mail.ru>
2026-06-05 21:42:33 +0300

    feat: добавлена история вычисленных выражений

--- src/app/app-calculator/app-calculator.ts
+++ src/app/app-calculator/app-calculator.ts
@@ -1,6 +1,7 @@
-import {Component, signal} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {History} from '../history/history';
+import {LogHistory} from '../log-history';
 
 @Component({
   selector: 'app-calculator',
@@ -15,8 +16,15 @@ export class AppCalculator {
   public operand_a = 0;
   public operand_b = 0;
   public operations = ["+", "-", "*", "/"]
-  public operation? = "+"
+  public operation = "+"
   public result = signal(0);
+  public historyService = inject(LogHistory);
+  public logCalculation = () => {
+    this.historyService.addHistory(this.operand_a, this.operand_b, this.operation, this.result())
+  }
+  public get hasHistory() {
+    return this.historyService.getAllHistory().length >= 1;
+  }
   public calculate = () => {
     switch (this.operation) {
       case ("+"): {
--- src/app/log-history.ts
+++ src/app/log-history.ts
@@ -5,11 +5,11 @@ import {History} from './shared/types/History';
   providedIn: 'root',
 })
 export class LogHistory {
-  private history: History[] = [{id: "1", expressionValue: "1 + 1 = 2"}];
+  private history: History[] = [];
   public getAllHistory = () => {
     return this.history;
   }
-  addHistory = (operand_a: string, operand_b: string, operation: string, result: number) => {
+  addHistory = (operand_a: number, operand_b: number, operation: string, result: number) => {
     this.history.push({
       id: crypto.randomUUID(),
       expressionValue: `${operand_a} ${operation} ${operand_b} = ${result}`
