64b9fb56a219395d4a665b7cc42f511118ddf7c6
oleg <oleg_09-95@mail.ru>
2026-06-06 21:44:19 +0300

    feat: сделан Header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import {AppCalculator} from './calculator/app-calculator/app-calculator';
-import {Navigation} from './navigation/navigation';
 import {RouterOutlet} from '@angular/router';
+import {Header} from './header/header';
 
 @Component({
   selector: 'app-root',
   imports: [
-    AppCalculator,
-    Navigation,
-    RouterOutlet
+    RouterOutlet,
+    Header
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
