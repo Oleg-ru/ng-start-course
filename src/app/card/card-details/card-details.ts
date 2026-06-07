a99e2ddf70facc97a5af56fe5efb60774c4ac6f2
oleg <oleg_09-95@mail.ru>
2026-06-07 19:59:58 +0300

    feat: сделан роутинг на детальную информацию по товару

--- src/app/card/card-details/card-details.ts
+++ src/app/card/card-details/card-details.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {Card} from '../types/card';
+import {cards} from '../data';
 
 @Component({
   selector: 'app-card-details',
@@ -6,4 +9,15 @@ import { Component } from '@angular/core';
   templateUrl: './card-details.html',
   styleUrl: './card-details.css',
 })
-export class CardDetails {}
+export class CardDetails {
+  public card?: Card
+
+  constructor(private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.card = cards.find(card => card.id === params['id'])
+    })
+  }
+}
