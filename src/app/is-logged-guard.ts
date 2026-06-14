27e87048bf992d6a5dfceae1dc0c1d5a7650b1fb
oleg <oleg_09-95@mail.ru>
2026-06-14 19:33:01 +0300

    feat: Guards начало

--- src/app/is-logged-guard.ts
+++ src/app/is-logged-guard.ts
@@ -1,5 +1,22 @@
-import { CanActivateFn } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateFn,
+  GuardResult,
+  MaybeAsync,
+  RouterStateSnapshot
+} from '@angular/router';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
 
+@Injectable({
+  providedIn: 'root',
+})
+export class isLoggedGuardService implements CanActivate {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> {
+    return false;
+  }
+}
 export const isLoggedGuard: CanActivateFn = (route, state) => {
   return true;
 };
