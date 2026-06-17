e2cca8800a7d025b4a220546d8bad77f570c15f8
oleg <oleg_09-95@mail.ru>
2026-06-17 21:03:51 +0300

    feat: -

--- src/app/user.ts
+++ src/app/user.ts
@@ -6,6 +6,7 @@ import {BehaviorSubject} from 'rxjs';
 })
 export class UserService {
   isUserLogger = new BehaviorSubject<boolean>(false);
+
   public login() {
     this.isUserLogger.next(true);
   }
